5429df8a10b1f5055a6c9a21f985e371889c0966
Homum Ahsan <hahsan95@gmail.com>
2018-08-23 16:37:50 -0500

    got seed data and api mostly working

--- script/seed.js
+++ script/seed.js
@@ -7,10 +7,10 @@ async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
 
-  // const users = await Promise.all([
-  //   User.create({email: 'cody@email.com', password: '123'}),
-  //   User.create({email: 'murphy@email.com', password: '123'})
-  // ])
+  const users = await Promise.all([
+    User.create({email: 'cody@email.com', password: '123'}),
+    User.create({email: 'murphy@email.com', password: '123'})
+  ])
 
   const frameworks = await Promise.all([
     Framework.create({name: 'React'}),
@@ -19,7 +19,7 @@ async function seed() {
     Framework.create({name: 'Vue'})
   ])
 
-  console.log(`seeded ${frameworks.length} frameworks`)
+  console.log(`seeded ${users.length} users!`)
   console.log(`seeded successfully`)
 }
 
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,6 @@
 const User = require('./user')
 const Framework = require('./framework')
+const getReactCommits = require('../../frameworks/react')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -16,5 +17,6 @@ const Framework = require('./framework')
  */
 module.exports = {
   User,
-  Framework
+  Framework,
+  getReactCommits
 }
