548eeb35fcdebdd969da4ece208e78458e568d18
Homum Ahsan <hahsan95@gmail.com>
2018-08-23 15:50:59 -0500

    model definition

---
+++ server/db/models/framework.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Framework = db.define('framework', {
+  name: {
+    type: Sequelize.STRING
+  }
+})
+
+module.exports = Framework
