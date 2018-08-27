18e0097d45047f4773891895c69f62f43530f5b1
Homum Ahsan <hahsan95@gmail.com>
2018-08-26 21:56:46 -0500

    making post routes

--- server/api/frameworks.js
+++ server/api/frameworks.js
@@ -12,3 +12,12 @@ router.get('/', async (req, res, next) => {
     console.error(err)
   }
 })
+
+router.post('/:frameworkId', async (req, res, next) => {
+  try {
+    let vote = await Framework.increment('votes', { where: {id: req.params.frameworkId} })
+    res.json(vote)
+  } catch(err) {
+    next(err)
+  }
+})
