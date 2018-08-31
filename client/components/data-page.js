a3d413bdc0bc1a12ba8a26c581b745bd3238934e
Homum Ahsan <hahsan95@gmail.com>
2018-08-31 04:54:08 -0500

    finally got data going to front end. next step, get graph visualizations and fix persistance problem. add about me page. add oauth or other login method, postgres heroku deployment

--- client/components/data-page.js
+++ client/components/data-page.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react'
 import AngularData from './angular-data'
+import ReactData from './react-data'
+import EmberData from './ember-data'
+import VueData from './vue-data'
 
 export default class DataPage extends Component {
   render() {
     return (
-      <div>bitchass</div>
+      <div>
+        bitchass
+        <AngularData />
+        <ReactData />
+        <EmberData />
+        <VueData />
+      </div>
     )
   }
 }
